1c7477c5a9dc36b3d8fb40e7528582fd92670bbb
strahlw <willstrahl32@gmail.com>
2023-06-30 12:48:39 -0400

    Removed jupyter syntax

--- molecool/functions.py
+++ molecool/functions.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
 
-%matplotlib notebook
-
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
     d=(rA-rB)
